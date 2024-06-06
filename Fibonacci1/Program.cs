d5ce355c4af455b2a7ad79d2c6d68bf2fd69d89f
ramon <ramonezorrilla@gmail.com>
2024-06-06 15:47:12 -0400

    Add project files.

---
+++ Fibonacci1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SERIE_DE_FIBONACCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte CAN, K;
+            int A, B, C;
+            string linea;
+            Console.Write("CUANTOS NÚMEROS: "); linea = Console.ReadLine();
+            CAN = byte.Parse(linea);
+            A = 1; B = 1;
+            Console.Write(A + " " + B + " ");
+            for (K = 3; K <= CAN; K++)
+            {
+                C = A + B;
+                Console.Write(C + " ");
+                //INTERCAMBIO DE VALORES
+                A = B;
+                B = C;
+            }
+            Console.WriteLine();
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
